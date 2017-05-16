6f79e6fa6cd1c6f16ba2609e3df8e48bddc053a6
user <234107@qq.com>
2017-05-17 00:28:24 +0800

    modify

--- renderer.js
+++ renderer.js
@@ -11,15 +11,25 @@ function encoder() {
     var fIn = "d:/user/desktop/myVideo.mp4";
     var fOut = "d:/user/desktop/myVideo_out.ts";
 
+    $("#outInfo").empty();
     const spawn = require('child_process').spawn;
     fProcess = spawn(f, ['-i', fIn, '-vcodec', 'libx264', '-acodec', 'mp2', '-f', 'mpegts', fOut, '-y']);
     //手动杀掉spawn,参见：https://discuss.atom.io/t/quitting-electron-app-no-process-exit-event-or-window-unload-event-on-renderer/27363
     fProcess.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
+        if (data.substr(0,6)=="frame="){
+            $("#outInfo").append(`${data}`);
+            $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
+        }
     });
 
     fProcess.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
+        console.log(typeof(data));
+        if (data.substr(0,6)=="frame=") {
+            $("#outInfo").append(`${data}`);
+            $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
+        }
     });
 
     fProcess.on('close', (code) => {
