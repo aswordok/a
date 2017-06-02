99a9d05d4acfd78028809678e927cec470109324
xzx.office <234107@qq.com>
2017-06-02 17:23:18 +0800

    modify

--- renderer.js
+++ renderer.js
@@ -7,7 +7,42 @@ console.log("Start render process");
 //f.exe  -i "d:/user/desktop/myVideo.ts" -vcodec libx264 -acodec mp2 -f mpegts "d:/user/desktop/myVideo_out.ts -y"
 //-y 输出覆盖
 let fProcess;
+let lastLogoAdd = "";
 function encoder() {
+    if ($("#checkLogo").prop("checked") && $("#logoAdd").val().trim().length > 0) {
+        const {app} = require('electron').remote;
+        let temp = app.getPath('temp');
+
+        const fs = require('fs');
+        const path = require('path');
+        let logoAdd = $("#logoAdd").val().trim();
+        logoTempAdd = path.join(temp, 'logo.png');
+        fs.exists(logoTempAdd, function (exists) {
+            if (exists) {
+                if (lastLogoAdd.length > 0 && lastLogoAdd === logoAdd) {
+                } else {
+                    fs.unlink(logoTempAdd, (err) => {
+                        if (err) {
+                            console.log("An error ocurred while delete the file " + logoTempAdd);
+                            console.log(err);
+                            return;
+                        }
+                        console.log(tmp + " has be deleted success.");
+                    });
+                    lastLogoAdd === logoAdd;
+                    cp();
+                }
+            } else {
+                tmpFiles.push(logoTempAdd);
+                lastLogoAdd === logoAdd;
+                cp(logoAdd,logoTempAdd);
+            }
+        });
+    }
+    function cp(logoAdd,logoTempAdd) {
+
+    }
+
     var fIn = "d:/user/desktop/myVideo.mp4";
     var fOut = "d:/user/desktop/myVideo_out.ts";
 
@@ -17,7 +52,7 @@ function encoder() {
     //手动杀掉spawn,参见：https://discuss.atom.io/t/quitting-electron-app-no-process-exit-event-or-window-unload-event-on-renderer/27363
     fProcess.stdout.on('data', (data) => {
         console.log(`stdout: ${data}`);
-        if (data.substr(0,6)=="frame="){
+        if (data.substr(0, 6) == "frame=") {
             $("#outInfo").append(`${data}`);
             $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
         }
@@ -26,7 +61,7 @@ function encoder() {
     fProcess.stderr.on('data', (data) => {
         console.log(`stderr: ${data}`);
         console.log(typeof(data));
-        if (data.substr(0,6)=="frame=") {
+        if (data.substr(0, 6) == "frame=") {
             $("#outInfo").append(`${data}`);
             $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
         }
