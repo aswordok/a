e4cecfb05366e412f4adf70833d0c06d7671dd16
user <234107@qq.com>
2017-06-04 19:04:23 +0800

    Merge branch 'master' of https://github.com/aswordok/a
    # Conflicts:
    #	renderer.js

--- renderer.js
+++ renderer.js
@@ -14,141 +14,148 @@ function encoder(data) {
         return;
     }
 
-    let adFullPre="";
-    let adFullPreIn="";
+    let adFullPre = "";
+    let adFullPreIn = "";
     if ($("#checkPre").prop("checked") && $("#preAdd").val().trim().length > 0) {
-        adFullPreIn=$("#preAdd").val().trim();
-        let adShorPre=adFullPreIn.substring(adFullPreIn.lastIndexOf("\\")+1,adFullPreIn.lastIndexOf("."));//文件名不带后缀
+        adFullPreIn = $("#preAdd").val().trim();
+        let adShorPre = adFullPreIn.substring(adFullPreIn.lastIndexOf("\\") + 1, adFullPreIn.lastIndexOf("."));//文件名不带后缀
         const {app} = require('electron').remote;
         let temp = app.getPath('temp');
         const path = require('path');
-        adFullPre = path.join(temp, adShorPre)+".ts";
-        console.log("adFullPre:")
-        console.log(adFullPre);
-    }else{
-        let adFullPre="";
-        let adFullPreIn="";
+        adFullPre = path.join(temp, adShorPre) + ".ts";
+        //console.log("adFullPre:")
+        //console.log(adFullPre);
+    } else {
+        let adFullPre = "";
+        let adFullPreIn = "";
     }
 
-    let adFullPost="";
-    let adFullPostIn="";
+    let adFullPost = "";
+    let adFullPostIn = "";
     if ($("#checkPost").prop("checked") && $("#postAdd").val().trim().length > 0) {
-        adFullPostIn=$("#postAdd").val().trim();
-        let adShorPost=adFullPostIn.substring(adFullPostIn.lastIndexOf("\\")+1,adFullPostIn.lastIndexOf("."));
+        adFullPostIn = $("#postAdd").val().trim();
+        let adShorPost = adFullPostIn.substring(adFullPostIn.lastIndexOf("\\") + 1, adFullPostIn.lastIndexOf("."));
         const {app} = require('electron').remote;
         let temp = app.getPath('temp');
         const path = require('path');
-        adFullPost = path.join(temp, adShorPost)+".ts";
-        console.log("adFullPost:")
-        console.log(adFullPost);
-    }else{
-        let adFullPost="";
-        let adFullPostIn="";
+        adFullPost = path.join(temp, adShorPost) + ".ts";
+        //console.log("adFullPost:")
+        //console.log(adFullPost);
+    } else {
+        let adFullPost = "";
+        let adFullPostIn = "";
     }
 
-    let mainFullPre="";
-    let mainFullIn="";
-    if (adFullPre.length>0 || adFullPost.length>0){
+    let mainFullPre = "";
+    let mainFullIn = "";
+    if (adFullPre.length > 0 || adFullPost.length > 0) {
         mainFullIn = $("#fileList option:first").val();
-        let mainShortIn = mainFullIn.substring(mainFullIn.lastIndexOf("\\")+1,mainFullIn.lastIndexOf("."));
-        let mainPathIn=mainFullIn.substr(0,mainFullIn.lastIndexOf("\\")+1);//含最后一个\
-        mainFullPre=mainPathIn+mainShortIn+"_pre.ts"
-        console.log("mainFullPre");
-        console.log(mainFullPre);
-    }else{
-        mainFullPre="";
-        mainFullIn="";
+        let mainShortIn = mainFullIn.substring(mainFullIn.lastIndexOf("\\") + 1, mainFullIn.lastIndexOf("."));
+        let mainPathIn = mainFullIn.substr(0, mainFullIn.lastIndexOf("\\") + 1);//含最后一个\
+        mainFullPre = mainPathIn + mainShortIn + "_pre.ts"
+        //console.log("mainFullPre");
+        //console.log(mainFullPre);
+    } else {
+        mainFullPre = "";
+        mainFullIn = "";
     }
 
-    let fFullIn="";
-    let fFullOut="";
-    let plain=true;
+    let fFullIn = "";
+    let fFullOut = "";
+    let plain = true;
     const fs = require('fs');
-    if (adFullPre.length>0 && !fs.existsSync(adFullPre)){//do pre
-        fFullIn=adFullPreIn;
-        fFullOut=adFullPre;
-        plain=true;
+    if (adFullPre.length > 0 && !fs.existsSync(adFullPre)) {//do pre
+        fFullIn = adFullPreIn;
+        fFullOut = adFullPre;
+        plain = true;
         tmpFiles.push(adFullPre);
-        console.log("Do adpre pretreatment.")
-    }else if (adFullPost.length>0 && !fs.existsSync(adFullPost)){//do post
-        fFullIn=adFullPostIn;
-        fFullOut=adFullPost;
-        plain=true;
+        console.log("Prepare for adpre.")
+    } else if (adFullPost.length > 0 && !fs.existsSync(adFullPost)) {//do post
+        fFullIn = adFullPostIn;
+        fFullOut = adFullPost;
+        plain = true;
         tmpFiles.push(adFullPost);
-        console.log("Do adpre pretreatment.")
-    }else if (mainFullPre.length>0 && !fs.existsSync(mainFullPre)){//do main pre
-        fFullIn=mainFullIn;
-        fFullOut=mainFullPre;
-        plain=false;
+        console.log("Prepare for adpost.")
+    } else if (mainFullPre.length > 0 && !fs.existsSync(mainFullPre)) {//do main pre
+        fFullIn = mainFullIn;
+        fFullOut = mainFullPre;
+        plain = false;
         //需要清理mainFullPre---------------------------
-        console.log("Do main pretreatment.")
-    }else{//直接编码或连接
-        let tmpIn=$("#fileList option:first").val();
+        console.log("Prepare for main.")
+    } else {//直接编码或连接
+        let tmpIn = $("#fileList option:first").val();
         $("#fileList option:first").remove();
-        let tmpShort=tmpIn.substring(tmpIn.lastIndexOf("\\")+1,tmpIn.lastIndexOf("."));
-        if (adFullPre.length==0 && adFullPost.length==0){//直接编码
+        let tmpShort = tmpIn.substring(tmpIn.lastIndexOf("\\") + 1, tmpIn.lastIndexOf("."));
+        let tmpPathIn = tmpIn.substr(0, tmpIn.lastIndexOf("\\") + 1);//含最后一个\
+        if (adFullPre.length == 0 && adFullPost.length == 0) {//直接编码
             fFullIn = tmpIn;
             const path = require('path');
-            fFullOut = path.join($("#outputAdd").val().trim(), tmpShort)+"_out.ts";
-            plain=false;
-            console.log("Start encoding the main file.")
-        }else{//连接
-            if (adFullPre.length>0){
-                fFullOut = "concat:" + adFullPre + "|";
-            }else{
-                fFullOut = "concat:";
+            fFullOut = path.join($("#outputAdd").val().trim(), tmpShort) + "_out.ts";
+            plain = false;
+            console.log("Start encoding the main file.");
+        } else {//连接
+            if (adFullPre.length > 0) {
+                fFullIn = "concat:" + adFullPre + "|";
+            } else {
+                fFullIn = "concat:";
             }
-            fFullOut+=
-
+            fFullIn += mainFullPre;
+            if (adFullPost.length > 0) {
+                fFullIn += "|" + adFullPost;
+            }
+            fFullOut = tmpPathIn + tmpShort + "_out.ts";
+            plain = true;
+            console.log("Start connecting the adpre+main+adpost file.");
         }
     }
+    //console.log("fFullIn:");
+    //console.log(fFullIn);
+    //console.log("fFullOut:");
+    //console.log(fFullOut);
+    fFullIn = "\"" + fFullIn + "\"";//测试支持空格？
+    fFullOut = "\"" + fFullOut + "\"";
 
-    /*let fFullIn = $("#fileList option:first").val();
-    console.log("fFullIn:")
-    console.log(fFullIn);
-    $("#fileList option:first").remove();
-
-    let fShortOut = fFullIn.replace(/^.+?\\([^\\]+?)(\.[^\.\\]*?)?$/gi, "$1");  //正则表达式获取文件名，不带后缀
-    const path = require('path');
-    fFullOut = path.join($("#outputAdd").val().trim(), fShortOut)+"_out.ts";
-    console.log("fFullOut:")
-    console.log(fFullOut);*/
-
+    if (plain) {
+        args = data.argsPlain;
+    } else {
+        args = data.argsMain;
+    }
+    args[0] = fFullIn;
+    args[args.length - 1] = fFullOut
+    console.log("args:");
+    console.log(args);
+    //act(args);
 }
 
-function act() {
-    /*
-     var fIn = "d:/user/desktop/myVideo.mp4";
-     var fOut = "d:/user/desktop/myVideo_out.ts";
+function act(args) {
+    $("#outInfo").empty();
+    const spawn = require('child_process').spawn; //HTML5的Web Worker是在客户端开线程的另一方法，示例：http://blog.jobbole.com/30592/
+    //args=['-i', fIn, '-vcodec', 'libx264', '-acodec', 'mp2', '-f', 'mpegts', fOut, '-y'];
+    fProcess = spawn(f, args);
+    //手动杀掉spawn,参见：https://discuss.atom.io/t/quitting-electron-app-no-process-exit-event-or-window-unload-event-on-renderer/27363
+    fProcess.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+        if (data.substr(0, 6) == "frame=") {
+            $("#outInfo").append(`${data}`);
+            $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
+        }
+    });
+    fProcess.stderr.on('data', (data) => {
+        console.log(`stderr: ${data}`);
+        console.log(typeof(data));
+        if (data.substr(0, 6) == "frame=") {
+            $("#outInfo").append(`${data}`);
+            $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
+        }
+    });
 
-     $("#outInfo").empty();
-     const spawn = require('child_process').spawn; //HTML5的Web Worker是在客户端开线程的另一方法，示例：http://blog.jobbole.com/30592/
-     fProcess = spawn(f, ['-i', fIn, '-vcodec', 'libx264', '-acodec', 'mp2', '-f', 'mpegts', fOut, '-y']);
-     //手动杀掉spawn,参见：https://discuss.atom.io/t/quitting-electron-app-no-process-exit-event-or-window-unload-event-on-renderer/27363
-     fProcess.stdout.on('data', (data) => {
-     console.log(`stdout: ${data}`);
-     if (data.substr(0, 6) == "frame=") {
-     $("#outInfo").append(`${data}`);
-     $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
-     }
-     });
-
-     fProcess.stderr.on('data', (data) => {
-     console.log(`stderr: ${data}`);
-     console.log(typeof(data));
-     if (data.substr(0, 6) == "frame=") {
-     $("#outInfo").append(`${data}`);
-     $("#outInfo").scrollTop($("#outInfo")[0].scrollHeight);
-     }
-     });
-
-     fProcess.on('close', (code) => {
-     console.log(`子进程退出码：${code}`);
-     });
-     */
+    fProcess.on('close', (code) => {
+        console.log(`完成，子进程退出码：${code}`);
+    });
 }
 
 exports.encoding = encoder;
+
 exports.killSpawn = () => {
     if (fProcess) {
         console.log('killing encoder ...');
